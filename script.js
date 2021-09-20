05b9ba64eaa6ed01ca5271ed295f64af0d947f69
Yash Kamaat <yashtkamaat@gmail.com>
2021-09-20 16:09:20 -0500

    Completed with Bonus

---
+++ script.js
@@ -0,0 +1,55 @@
+// JavaScript Basics Exercise
+// --------------------
+// 1
+console.log(13);
+// --------------------
+// 2
+//  Booleans, String, Numbers, Null, Undefined
+// --------------------
+// 3
+// %, +, -, /, *, 
+// --------------------
+// 4
+console.log(5%2);
+console.log(5%4);
+// 4 into 1 with one remainder, 2 into 5 2x's with 1 remainder. 5/4=1, 2+2=4 with 1 remainder
+// --------------------
+// 5
+console.log('I\'M Mary Poppins Y\'ALL!'  )
+// --------------------
+// 6
+console.log("Hawk" + "Eye");
+// --------------------
+// 7
+console.log("Yash".length + "Kamaat".length);
+// --------------------
+// 8
+console.log("Avengers Endgame"[15]);
+
+// Bonus
+// 1
+console.log ("//")
+// -----------------
+// 2
+console.log (5**4-9)
+// 5*5*5*5=625 625-9=616
+// ----------------
+// 3
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+console.log(("I".length + "Am".length * "Iron".length) % "Man".length);
+// ----------------
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+// -----------------
+// 5
+var m =25
+var c =4
+var u = m + c;
+console.log (u);
