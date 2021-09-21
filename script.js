b3f5a95348c4db6a3fc549ae3c6bb4f71daa37df
Yash Kamaat <yashtkamaat@gmail.com>
2021-09-21 10:02:40 -0500

    Added additional notes

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ console.log(13);
 //  Booleans, String, Numbers, Null, Undefined
 // --------------------
 // 3
-// %, +, -, /, *, 
+// %, +, -, /, *, ** 
 // --------------------
 // 4
 console.log(5%2);
 console.log(5%4);
+// console.log(1%5);
 // 4 into 1 with one remainder, 2 into 5 2x's with 1 remainder. 5/4=1, 2+2=4 with 1 remainder
 // --------------------
 // 5
@@ -24,7 +25,7 @@ console.log("Hawk" + "Eye");
 console.log("Yash".length + "Kamaat".length);
 // --------------------
 // 8
-console.log("Avengers Endgame"[15]);
+console.log("Avengers: Endgame"[16]);
 
 // Bonus
 // 1
@@ -35,8 +36,14 @@ console.log (5**4-9)
 // 5*5*5*5=625 625-9=616
 // ----------------
 // 3
-console.log("I".length - "Am".length + "Iron".length - "Man".length);
+// console.log("I".length - "Am".length + "Iron".length - "Man".length);
+// or
+// --------------
 console.log(("I".length + "Am".length * "Iron".length) % "Man".length);
+
+// or
+// ----------------
+// console.log(("I".length + "Am".length) * "Iron".length / "Man".length % 4);
 // ----------------
 // 4
 console.log("CAP"[1]);
@@ -48,6 +55,11 @@ console.log("DOCTOR STRANGE"[13]);
 console.log("THOR"[3]);
 console.log("STAR-LORD"[0]);
 // -----------------
+// or
+console.log("CAP"[1] + "\n" + "VISION"[0] + "\n" + "OKOYE"[4] + "\n" + "NICK FURY"[0] + "\n" + "GROOT"[0] + "\n" + "DOCTOR STRANGE"[13] + "\n" + "THOR"[3] + "\n" + "STAR-LORD"[0]);
+// or
+// console.table(["CAP"[1], "VISION"[0], "OKOYE"[4], "NICK FURY"[0], "GROOT"[0], "DOCTOR STRANGE"[13], "THOR"[3], "STAR-LORD"[0]]);
+// ---------------------------
 // 5
 var m =25
 var c =4
